a8baf0faa38ee8ccfe0d63c3f278ad734aaaf30d
fanhaiqiu <944080287@qq.com>
2020-01-06 21:28:54 +0800

    init

--- src/main/java/com/fanhq/example/problem/CalculateStringLength.java
+++ src/main/java/com/fanhq/example/problem/CalculateStringLength.java
@@ -1,11 +1,19 @@
 package com.fanhq.example.problem;
 
+import java.util.Scanner;
+
 /**
  * @author fanhaiqiu
  * @date 2020/1/6
  */
 public class CalculateStringLength {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input == null) {
+            return;
+        }
+        String[] array = input.split(" ");
+        System.out.println(array[array.length -1].length());
     }
 }
