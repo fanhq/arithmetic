0400bd0dbf62acf0485d38bf984cfba47747ad50
fanhaiqiu <944080287@qq.com>
2020-01-06 21:18:33 +0800

    init

--- src/main/java/com/fanhq/example/Application.java
+++ src/main/java/com/fanhq/example/Application.java
@@ -1,10 +1,16 @@
 package com.fanhq.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            String s = input.nextLine();
+            System.out.println(s);
+        }
     }
 }
