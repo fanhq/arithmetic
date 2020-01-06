1709a24bc946bf39223c33d155d65c506113f577
fanhaiqiu <944080287@qq.com>
2020-01-06 21:21:08 +0800

    init

---
+++ src/main/java/com/fanhq/example/problem/CalculateStringLength.java
@@ -0,0 +1,11 @@
+package com.fanhq.example.problem;
+
+/**
+ * @author fanhaiqiu
+ * @date 2020/1/6
+ */
+public class CalculateStringLength {
+    public static void main(String[] args) {
+
+    }
+}
