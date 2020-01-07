19cebae0ced157a7104d7eb2e80c508fa60584b3
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2020-01-07 16:50:24 +0800

    init

--- src/main/java/com/fanhq/example/Application.java
+++ src/main/java/com/fanhq/example/Application.java
@@ -1,16 +1,19 @@
 package com.fanhq.example;
 
-import java.util.Scanner;
-
 /**
  * Hello world!
  */
 public class Application {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        while (input.hasNext()) {
-            String s = input.nextLine();
-            System.out.println(s);
-        }
+//        Scanner input = new Scanner(System.in);
+//        while (input.hasNext()) {
+//            String s = input.nextLine();
+//            System.out.println(s);
+//        }
+
+        System.out.println(Math.ceil(7/3));
+        System.out.println((float) 7/3);
+        System.out.println(Math.round(3.5));
+        System.out.println(Math.floor(1.0));
     }
 }
