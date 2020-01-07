04fb6f8dcbf5a30fb7401608fed858e79a5db764
fanhaiqiu <944080287@qq.com>
2020-01-07 20:51:17 +0800

    init

--- src/main/java/com/fanhq/example/problem/ExchangeDemo.java
+++ src/main/java/com/fanhq/example/problem/ExchangeDemo.java
@@ -24,41 +24,41 @@ public class ExchangeDemo {
     private static int exchangeWays(int[] arraydemo, int aim) {
         // TODO Auto-generated method stub
 
-        if(arraydemo==null || arraydemo.length==0 || aim<0) {
+        if (arraydemo == null || arraydemo.length == 0 || aim < 0) {
             return -1;
         }
 
-        int n=arraydemo.length;
-        int max=Integer.MAX_VALUE;
-        int[][] dp=new int[n][aim+1];
-        for(int j=1;j<=aim;j++) {
-            dp[0][j]=max;
-            if(j-arraydemo[0]>=0 && dp[0][j-arraydemo[0]]!=max) {
-                dp[0][j]=dp[0][j-arraydemo[0]]+1;
+        int n = arraydemo.length;
+        int max = Integer.MAX_VALUE;
+        int[][] dp = new int[n][aim + 1];
+        for (int j = 1; j <= aim; j++) {
+            dp[0][j] = max;
+            if (j - arraydemo[0] >= 0 && dp[0][j - arraydemo[0]] != max) {
+                dp[0][j] = dp[0][j - arraydemo[0]] + 1;
             }
         }
 
-        int left=0;
-        for(int i=1;i<n;i++) {
-            for(int j=1;j<=aim;j++) {
-                left=max;
-                if(j-arraydemo[i]>=0 && dp[i][j-arraydemo[i]]!=max) {
-                    left=dp[i][j-arraydemo[i]]+1;
+        int left = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= aim; j++) {
+                left = max;
+                if (j - arraydemo[i] >= 0 && dp[i][j - arraydemo[i]] != max) {
+                    left = dp[i][j - arraydemo[i]] + 1;
                 }
-                dp[i][j]=Math.min(left, dp[i-1][j]);
+                dp[i][j] = Math.min(left, dp[i - 1][j]);
             }
         }
 
 
-        return dp[n-1][aim] != max? dp[n-1][aim]:-1;
+        return dp[n - 1][aim] != max ? dp[n - 1][aim] : -1;
     }
 
     public static void main(String[] args) {
-        int arrayDemo[]= {1,5,10,50,100};
-        int aim=57;
+        int arrayDemo[] = {1, 5, 10, 50, 100};
+        int aim = 57;
 
-        int answer= exchangeWays(arrayDemo,aim);
-        System.out.println("# ANSWER= "+answer);
+        int answer = exchangeWays(arrayDemo, aim);
+        System.out.println("# ANSWER= " + answer);
 
     }
 }
