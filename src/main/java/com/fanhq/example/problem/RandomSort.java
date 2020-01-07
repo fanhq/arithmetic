b896f5922391af6302a16f3755ec3c757cf684d8
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2020-01-07 15:29:46 +0800

    init

--- src/main/java/com/fanhq/example/problem/RandomSort.java
+++ src/main/java/com/fanhq/example/problem/RandomSort.java
@@ -1,36 +1,54 @@
 package com.fanhq.example.problem;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 /**
  * @author fanhaiqiu
  * @date 2020/1/6
  */
 public class RandomSort {
+    //    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        while(scanner.hasNext()){
+//            int count = scanner.nextInt();
+//            List<Integer> array = new ArrayList<>();
+//            for (int i = 0; i < count; i++) {
+//                int val = scanner.nextInt();
+//                if (array.contains(val)){
+//                    continue;
+//                }
+//                array.add(val);
+//            }
+//            array.sort(new Comparator<Integer>() {
+//                @Override
+//                public int compare(Integer o1, Integer o2) {
+//                    if (o1 > o2) {
+//                        return 1;
+//                    }
+//                    return -1;
+//                }
+//            });
+//            array.forEach(x->{
+//                System.out.println(x);
+//            });
+//        }
+//    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNext()){
+        while (scanner.hasNext()) {
             int count = scanner.nextInt();
-            List<Integer> array = new ArrayList<>();
+            TreeSet<Integer> set = new TreeSet<Integer>();
             for (int i = 0; i < count; i++) {
-                int val = scanner.nextInt();
-                if (array.contains(val)){
-                    continue;
-                }
-                array.add(val);
+                int curr = scanner.nextInt();
+                set.add(curr);
             }
-            array.sort(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    if (o1 > o2) {
-                        return 1;
-                    }
-                    return -1;
-                }
-            });
-            array.forEach(x->{
-                System.out.println(x);
-            });
+            for (Integer i : set) {
+                System.out.println(i);
+            }
+
         }
     }
+
 }
