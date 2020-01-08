fae06457237f119f23caa116ccd65bfeb1d223e3
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2020-01-08 18:20:49 +0800

    init

--- src/main/java/com/fanhq/example/Application.java
+++ src/main/java/com/fanhq/example/Application.java
@@ -5,15 +5,8 @@ package com.fanhq.example;
  */
 public class Application {
     public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        while (input.hasNext()) {
-//            String s = input.nextLine();
-//            System.out.println(s);
-//        }
 
-        System.out.println(Math.ceil(7/3));
-        System.out.println((float) 7/3);
-        System.out.println(Math.round(3.5));
-        System.out.println(Math.floor(1.0));
     }
 }
+
+
---
+++ src/main/java/com/fanhq/example/problem/Solution.java
@@ -0,0 +1,42 @@
+package com.fanhq.example.problem;
+
+/**
+ * @author fanhaiqiu
+ * @date 2020/1/8
+ */
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * s3 是否是由 s1 和 s2 交错组成
+     *
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return
+     */
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if ((s1.length() + s2.length()) != s3.length()) {
+            return false;
+        }
+        boolean[] dp = new boolean[s2.length()];
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                }else if (i == 0) {
+                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                } else if (j == 0) {
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                } else {
+                    dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+        return dp[s2.length()];
+    }
+}
