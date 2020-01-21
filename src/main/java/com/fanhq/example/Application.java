fe7d855523cea4afd19b274e3b392e65f4d30f24
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2020-01-21 09:18:30 +0800

    init

--- src/main/java/com/fanhq/example/Application.java
+++ src/main/java/com/fanhq/example/Application.java
@@ -1,25 +1,12 @@
 package com.fanhq.example;
 
-import com.fanhq.example.problem.LRUCache;
-
 /**
  * Hello world!
  */
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        LRUCache<String, String> cache = new LRUCache<>(5);
-        cache.put("1", "1");
-        cache.put("2", "2");
-        cache.put("3", "3");
-        cache.put("4", "4");
-        cache.put("5", "5");
-        cache.get("1");
-        cache.put("6", "6");
-        cache.put("7", "7");
-        cache.forEach((k, v) -> {
-            System.out.println(v);
-        });
+
     }
 }
 
