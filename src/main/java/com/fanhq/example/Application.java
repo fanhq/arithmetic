1e83e9607bc2ca8b122462a87426ad796b962de3
fanhaiqiu <944080287@qq.com>
2020-04-18 13:07:24 +0800

    init

--- src/main/java/com/fanhq/example/Application.java
+++ src/main/java/com/fanhq/example/Application.java
@@ -2,6 +2,12 @@ package com.fanhq.example;
 
 import com.fanhq.example.leetcode.LeetCode;
 import com.fanhq.example.leetcode.ListNode;
+import jdk.nashorn.internal.ir.IfNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -9,17 +15,7 @@ import com.fanhq.example.leetcode.ListNode;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
 
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-
-        LeetCode leetCode = new LeetCode();
-        ListNode l = leetCode.addTwoNumbers(l1, l2);
-        assert l != null;
 
     }
 
