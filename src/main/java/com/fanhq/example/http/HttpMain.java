d81c11c702b295ef23385e76de2d9bd7f3f13070
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2020-06-06 16:40:11 +0800

    init

--- src/main/java/com/fanhq/example/http/HttpMain.java
+++ src/main/java/com/fanhq/example/http/HttpMain.java
@@ -1,11 +1,16 @@
 package com.fanhq.example.http;
 
-import javax.net.ssl.*;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.TrustManagerFactory;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.SecureRandom;
@@ -20,37 +25,35 @@ public class HttpMain {
     public static void main(String[] args) throws Exception {
 
         SSLContext sslContext = sslContext("C:\\Users\\Lenovo\\ssl_server.jks", "Iot@2020");
-//        SSLParameters sslParameters = new SSLParameters();
-//        String responseBody = HttpClient.newBuilder().sslContext(sslContext).sslParameters(sslParameters).build()
-//                .send(HttpRequest.newBuilder(new URI("https://127.0.0.1:9003/demo/receive"))
-//                        .GET()
-//                        .build(), HttpResponse.BodyHandlers.ofString()).body();
-//        System.out.println(responseBody);
-
-
+        SSLParameters sslParameters = new SSLParameters();
+        String responseBody = HttpClient.newBuilder().sslContext(sslContext).sslParameters(sslParameters).build()
+                .send(HttpRequest.newBuilder(new URI("https://127.0.0.1:9003/demo/receive"))
+                        .GET()
+                        .build(), HttpResponse.BodyHandlers.ofString()).body();
+        System.out.println(responseBody);
 
 
         //https
-        HostnameVerifier allHostsValid = new HostnameVerifier() {
-            public boolean verify(String hostname, SSLSession session) {
-                return true;
-            }
-        };
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-        URL url = new URL("https://127.0.0.1:9003/demo/receive");
-        // 打开restful链接
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");// POST GET PUT DELETE
-        // 设置访问提交模式，表单提交
-        conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
-        conn.setConnectTimeout(130000);// 连接超时 单位毫秒
-        conn.setReadTimeout(130000);// 读取超时 单位毫秒
-        // 读取请求返回值
-        byte bytes[] = new byte[1024];
-        InputStream inStream = conn.getInputStream();
-        inStream.read(bytes, 0, inStream.available());
-        System.out.println(new String(bytes, "utf-8"));
+//        HostnameVerifier allHostsValid = new HostnameVerifier() {
+//            public boolean verify(String hostname, SSLSession session) {
+//                return true;
+//            }
+//        };
+//        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+//        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+//        URL url = new URL("https://127.0.0.1:9003/demo/receive");
+//        // 打开restful链接
+//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//        conn.setRequestMethod("GET");// POST GET PUT DELETE
+//        // 设置访问提交模式，表单提交
+//        conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+//        conn.setConnectTimeout(130000);// 连接超时 单位毫秒
+//        conn.setReadTimeout(130000);// 读取超时 单位毫秒
+//        // 读取请求返回值
+//        byte bytes[] = new byte[1024];
+//        InputStream inStream = conn.getInputStream();
+//        inStream.read(bytes, 0, inStream.available());
+//        System.out.println(new String(bytes, "utf-8"));
     }
 
 
