da7a1ee6fdee48fbb98db057764cb8df3dbce160
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2021-06-17 11:18:38 +0800

    init

--- src/main/java/com/fanhq/example/kafka/CustomAdmin.java
+++ src/main/java/com/fanhq/example/kafka/CustomAdmin.java
@@ -1,6 +1,5 @@
 package com.fanhq.example.kafka;
 
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.ListConsumerGroupOffsetsResult;
@@ -18,7 +17,7 @@ public class CustomAdmin {
         Properties props = new Properties();
         props.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "172.19.3.194:9092,172.19.3.195:9092,172.19.3.196:9092");
         props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"admin\" password=\"iot@10086\";");
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        props.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
         props.put(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-512");
         props.put("request.timeout.ms", 600000);
         AdminClient adminClient = AdminClient.create(props);
