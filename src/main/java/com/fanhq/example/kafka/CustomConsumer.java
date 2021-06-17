231c81db9ed041bda4c8afc3b09772c96f5846b6
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2021-06-17 09:45:33 +0800

    init

---
+++ src/main/java/com/fanhq/example/kafka/CustomConsumer.java
@@ -0,0 +1,34 @@
+package com.fanhq.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author: fanhaiqiu
+ * @date: 2021/6/17
+ */
+public class CustomConsumer {
+
+    public static void main(String[] args) throws Exception{
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "172.19.3.194:9092,172.19.3.195:9092,172.19.3.196:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("test"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records){
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
---
+++ src/main/java/com/fanhq/example/kafka/CustomProducer.java
@@ -0,0 +1,45 @@
+package com.fanhq.example.kafka;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.config.SaslConfigs;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: fanhaiqiu
+ * @date: 2021/6/16
+ */
+public class CustomProducer {
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "172.19.3.194:9092,172.19.3.195:9092,172.19.3.196:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("linger.ms", 1);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"fanhaiqiu\" password=\"iot@10086\";");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        props.put(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-512");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)), new Callback() {
+
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    exception.printStackTrace();
+                    System.out.println(metadata.toString());
+                }
+            });
+        }
+        TimeUnit.SECONDS.sleep(3);
+        producer.close();
+    }
+
+
+}
