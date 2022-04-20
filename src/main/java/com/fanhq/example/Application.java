68aa0950340ce6e309c9e2d6cee2d4c6e4034005
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2022-04-20 08:58:07 +0800

    init

--- src/main/java/com/fanhq/example/Application.java
+++ src/main/java/com/fanhq/example/Application.java
@@ -11,9 +11,11 @@ import reactor.netty.Connection;
 import reactor.netty.DisposableServer;
 import reactor.netty.tcp.TcpServer;
 
+import javax.sound.midi.Soundbank;
 import java.time.ZoneOffset;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.*;
 
@@ -28,10 +30,27 @@ public class Application {
     //  private final ThreadLocal<NashornSandbox> threadLocal = ThreadLocal.withInitial(this::createNashornSandbox);
 
     public static void main(String[] args) throws Exception {
-
-
+        String a= "设备号:15,起始频率:1550,结束频率:1620,功放:关闭,温度:26.7,正向功率:-1.22,反向功率:2.1,电流:0,电压:27.25,驻波比:1.2|设备号:24,起始频率:2395,结束频率:2495,功放:关闭,温度:31.7,正向功率:-16.5,反向功率:-2.05,电流:0.18,电压:27.54,驻波比:1.2|设备号:58,起始频率:5715,结束频率:5850,功放:关闭,温度:28.6,正向功率:-2.34,反向功率:-2.54,电流:0.25,电压:27.3,驻波比:1.2";
+        String[] split = a.split("\\|");
+        System.out.println(split.length);
+        //System.out.println(11>11);
+        //int sum = 0;
+        //int time = 11;
+        //for (int i = 1; i< time; i++){
+        //    int skipCycle4NextSend = getSkipCycle4NextSend(i);
+        //    sum = sum +skipCycle4NextSend;
+        //}
+        //System.out.println(getSkipCycle4NextSend(10));
+        //System.out.println(sum);
     }
 
+    public static String byteToHex(byte b) {
+        String hex = Integer.toHexString(b & 0xFF);
+        if (hex.length() < 2) {
+            hex = "0" + hex;
+        }
+        return hex;
+    }
 
     public static int getSkipCycle4NextSend(int sentTimes) {
         int sum = 0;
---
+++ src/main/java/com/fanhq/example/netty/NettyClient.java
@@ -0,0 +1,90 @@
+package com.fanhq.example.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Hachel on 2018/10/25
+ */
+public class NettyClient {
+
+    public static void main(String[] args) {
+        // 客户端只需要一个EventLoopGroup
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class).handler(
+                    new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            //pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+                            //pipeline.addLast(new LengthFieldPrepender(4));
+                            pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+                            pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+                            // 最后添加我们自己的处理器
+                            pipeline.addLast(new MyClientHandler());
+                        }
+                    }
+            );
+
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 8899).sync();
+           // ByteBuf content = Unpooled.copiedBuffer("i am client", CharsetUtil.UTF_8);
+            // 发送客户端的请求
+            Channel channel = channelFuture.channel();
+            channel.writeAndFlush("i am client");
+
+            TimeUnit.SECONDS.sleep(10);
+            channel.writeAndFlush("i am client");
+
+            TimeUnit.SECONDS.sleep(10);
+            ChannelFuture channelFuture1 = channel.writeAndFlush("i am client");
+            System.out.println(channelFuture.isSuccess());
+
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+
+    public static class MyClientHandler extends ChannelDuplexHandler {
+
+        //@Override
+        //protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        //    //System.out.println(ctx.channel().remoteAddress());
+        //    System.out.println("server response: " + msg);
+        //    //ctx.writeAndFlush("from clinet: " + LocalDateTime.now());
+        //}
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            InetSocketAddress socketAddress = (InetSocketAddress) ctx.channel().remoteAddress();
+            super.channelActive(ctx);
+            System.out.println("channelActive");
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            super.channelInactive(ctx);
+            System.out.println("channelInactive");
+        }
+    }
+}
